90a9a0f15ebbdf9498e4fd04fb80222261fdc4ec
Bruno Ricardo de Menezes <bruno.menezes@iti.gov.br>
2024-04-16 14:30:21 -0300

    Arquivos do projeto

---
+++ src/main/java/com/example/acfuba/AcFubaApplication.java
@@ -0,0 +1,13 @@
+package com.example.acfuba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcFubaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcFubaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/acfuba/AcFubaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acfuba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcFubaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
